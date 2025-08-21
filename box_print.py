a27d07887f65aeb9114d1125fa606d9d9c7314b0
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-08-21 10:24:25 -0600

    Create box_print()

--- box_print.py
+++ box_print.py
@@ -1,1 +1,17 @@
-
+def box_print(symbol, width, height):
+    """
+    Prints a box made of the given symbol, with specified width and height.
+    The box has the symbol as borders and spaces inside.
+    """
+    
+    # Validate that the symbol is a single character
+    if len(symbol) != 1:
+        raise Exception('Symbol must be a single character string.')
+    
+    # Validate that the width is large enough to form a box
+    if width <= 2:
+        raise Exception('Width must be greater than 2.')
+    
+    # Validate that the height is large enough to form a box
+    if height <= 2:
+        raise Exception('Height must be greater than 2.')
